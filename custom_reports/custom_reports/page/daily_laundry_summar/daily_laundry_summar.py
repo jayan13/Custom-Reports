f44bb61303682e6cdb62446a5ca54c6b4c580d6a
jayan13 <jayakumar13s@gmail.com>
2022-07-28 16:58:27 +0530

    general report update

--- custom_reports/custom_reports/page/daily_laundry_summar/daily_laundry_summar.py
+++ custom_reports/custom_reports/page/daily_laundry_summar/daily_laundry_summar.py
@@ -16,7 +16,7 @@ def get_report(customer=None,from_date=None,to_date=None):
     data['to_date']=getdate(to_date).strftime('%m/%d/%Y')
     data['head']=frappe.db.get_value('Letter Head', 'Quick Laundry Letter Head', 'content')
    
-    data['items']=frappe.db.sql("""select si.po_no,si.name,DATE_FORMAT(si.posting_date,'%d/%m/%Y') AS posting_date,si.total_qty,si.total,si.total_taxes_and_charges,si.grand_total from `tabSales Invoice` si left join `tabAddress` ad on ad.name=si.customer_address where si.company='Quick Laundry – Sole Proprietorship LLC' and si.docstatus=1 and si.customer='{0}' and (si.posting_date between '{1}' and '{2}')  """.format(customer,from_date,to_date),as_dict=1,debug=0)
+    data['items']=frappe.db.sql("""select si.po_no,si.name,DATE_FORMAT(si.posting_date,'%d/%m/%Y') AS posting_date,si.total_qty,si.total,si.total_taxes_and_charges,si.grand_total from `tabSales Invoice` si left join `tabAddress` ad on ad.name=si.customer_address where si.company='Quick Laundry – Sole Proprietorship LLC' and si.docstatus=1 and si.customer='{0}' and (si.posting_date between '{1}' and '{2}')  order by si.posting_date""".format(customer,from_date,to_date),as_dict=1,debug=0)
 
     data['items_total']=frappe.db.sql("""select sum(si.total_qty) as total_qty,sum(si.total) as total,sum(si.total_taxes_and_charges) as total_taxes,sum(si.grand_total) as  grand_total from `tabSales Invoice` si left join `tabAddress` ad on ad.name=si.customer_address where si.company='Quick Laundry – Sole Proprietorship LLC' and si.docstatus=1 and si.customer='{0}' and (si.posting_date between '{1}' and '{2}') group by si.customer """.format(customer,from_date,to_date),as_dict=1,debug=0)
     
