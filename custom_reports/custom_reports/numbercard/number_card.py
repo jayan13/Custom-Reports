e968a8f3637311bfe07a28f8f95c4f37afde2269
jayan13 <jayakumar13s@gmail.com>
2022-08-17 23:46:50 +0530

    number card methods

--- custom_reports/custom_reports/numbercard/number_card.py
+++ custom_reports/custom_reports/numbercard/number_card.py
@@ -12,7 +12,7 @@ AND `tabGL Entry`.company = 'ABU DHABI POULTRY FARM - SOLE PROPRIETORSHIP L.L.C.
 AND `tabGL Entry`.account = '114201 - CASH SALES - SHJ - APF' """,as_dict=1)[0]
    
     if balance:
-        data['value']=balance.account_Balance
+        data['value']=round(balance.account_Balance,2)
     data['fieldtype']='Float'
     return data
 
@@ -28,7 +28,7 @@ AND `tabGL Entry`.company = 'ABU DHABI POULTRY FARM - SOLE PROPRIETORSHIP L.L.C.
 AND `tabGL Entry`.account = '114202 - CASH SALES - AUH - APF' """,as_dict=1)[0]
    
     if balance:
-        data['value']=balance.account_Balance
+        data['value']=round(balance.account_Balance,2)
     data['fieldtype']='Float'
     return data
 
@@ -44,7 +44,7 @@ AND `tabGL Entry`.company = 'ABU DHABI POULTRY FARM - SOLE PROPRIETORSHIP L.L.C.
 AND `tabGL Entry`.account = '114203 - CASH SALES - AL AIN - APF' """,as_dict=1)[0]
    
     if balance:
-        data['value']=balance.account_Balance
+        data['value']=round(balance.account_Balance,2)
     data['fieldtype']='Float'
     return data
 
@@ -60,6 +60,6 @@ AND `tabGL Entry`.company = 'ABU DHABI POULTRY FARM - SOLE PROPRIETORSHIP L.L.C.
 AND `tabGL Entry`.account = '114205 - EGG CASH SALE - FARM - APF' """,as_dict=1)[0]
    
     if balance:
-        data['value']=balance.account_Balance
+        data['value']=round(balance.account_Balance,2)
     data['fieldtype']='Float'
     return data
