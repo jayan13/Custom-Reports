60c4354054dc3889a7a3efac890209558886d882
jayan13 <jayakumar13s@gmail.com>
2022-08-30 00:48:50 +0530

    property report pages

---
+++ custom_reports/custom_reports/page/villas/villas.js
@@ -0,0 +1,56 @@
+frappe.pages['villas'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Property Unit Report',
+		single_column: true
+	});
+}
+
+
+frappe.pages['villas'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Property Unit Report',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				
+				this.page.add_inner_button('Print', () => print_rep());
+				var wrp=this.page.main
+				frappe.call({
+					method: 'custom_reports.custom_reports.page.villas.villas.get_report',
+					freeze: 1,
+					freeze_message: 'Data loading ...please waite',					
+					callback: function (r) {
+					  if (r.message) {							
+							$(frappe.render_template("villas",r.message)).appendTo(wrp);	
+					  }
+					},
+				  });
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('unit_report');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
