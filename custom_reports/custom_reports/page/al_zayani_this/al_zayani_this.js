7197edf355de47ca38063f767d7c6bc343bd126a
jayan13 <jayakumar13s@gmail.com>
2022-09-12 23:34:22 +0530

    property dash detail pages

---
+++ custom_reports/custom_reports/page/al_zayani_this/al_zayani_this.js
@@ -0,0 +1,49 @@
+
+
+frappe.pages['al-zayani-this'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Property Unit Report',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				
+				this.page.add_inner_button('Print', () => print_rep());
+				var wrp=this.page.main
+				frappe.call({
+					method: 'custom_reports.custom_reports.page.al_zayani_this.al_zayani_this.get_report',
+					freeze: 1,
+					freeze_message: 'Data loading ...please waite',					
+					callback: function (r) {
+					  if (r.message) {							
+							$(frappe.render_template("al_zayani_building",r.message)).appendTo(wrp);	
+					  }
+					},
+				  });
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('unit_report');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
---
+++ custom_reports/custom_reports/page/musaffah_next/musaffah_next.py
@@ -0,0 +1,16 @@
+import frappe
+from frappe.utils import formatdate, getdate
+
+@frappe.whitelist()
+def get_report():
+    data = {}    
+    
+    frappe.db.sql(""" set @rownum := 0 """)
+    data['items']=frappe.db.sql("""select @rownum := @rownum + 1 as row_number,property_name,name,customer_name,DATE_FORMAT(contract_start_date,'%d/%m/%Y') as contract_start_date,DATE_FORMAT(contract_end_date,'%d/%m/%Y') as contract_end_date,unit_status from `tabProperty Unit` where MONTH(DATE_SUB(contract_end_date,INTERVAL 1 MONTH))=MONTH(CURRENT_DATE()) AND YEAR(DATE_SUB(contract_end_date,INTERVAL 1 MONTH))=YEAR(CURRENT_DATE()) and property_name like '%Musaffah Plot%' """,as_dict=1,debug=0)
+    return data
+
+       
+    
+
+    
+    
