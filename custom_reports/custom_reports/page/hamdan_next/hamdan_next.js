b1a8c8e228baea0b52c3edb60cdf86da3f63c512
jayan13 <jayakumar13s@gmail.com>
2022-09-13 10:30:40 +0530

    dure production report

--- custom_reports/custom_reports/page/hamdan_next/hamdan_next.js
+++ custom_reports/custom_reports/page/hamdan_next/hamdan_next.js
@@ -1,7 +1,48 @@
+
 frappe.pages['hamdan-next'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Property Unit Report',
-		single_column: true
-	});
+	new MyPage(wrapper);
 }
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Property Unit Report',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				
+				this.page.add_inner_button('Print', () => print_rep());
+				var wrp=this.page.main
+				frappe.call({
+					method: 'custom_reports.custom_reports.page.hamdan_next.hamdan_next.get_report',
+					freeze: 1,
+					freeze_message: 'Data loading ...please waite',					
+					callback: function (r) {
+					  if (r.message) {							
+							$(frappe.render_template("hamdan_building",r.message)).appendTo(wrp);	
+					  }
+					},
+				  });
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('unit_report');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
