caedfe2a07886165562765caa82bfbe795fe2811
jayan13 <jayakumar13s@gmail.com>
2022-09-29 19:55:44 +0530

    ql sale invoice print

---
+++ custom_reports/custom_reports/page/ql_sales_invoice_con/ql_sales_invoice_con.js
@@ -0,0 +1,147 @@
+frappe.pages['ql-sales-invoice-con'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Sales Invoice',
+		single_column: true
+	});
+}
+
+frappe.pages['ql-sales-invoice-con'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Sales Invoice',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				const comarry=[];
+				
+				
+				
+					let field = this.page.add_field({
+					label: 'Customer',
+					fieldtype: 'Select',
+					fieldname: 'customer',
+					options: [],
+					change() {
+						get_invoice();
+					}
+				});
+				
+				frappe.call({
+				  method: 'custom_reports.custom_reports.page.ql_sales_invoice_con.ql_sales_invoice_con.get_customer_list',
+				  args: {},
+				  callback: function (r) {
+					if (r.message) {
+						$('[data-fieldname="customer"][type="text"]').append($("<option></option>").attr("value", "").text("")); 
+						$.each( r.message.customers, function( key, value ) {					
+							
+							$('[data-fieldname="customer"][type="text"]').append($("<option></option>").attr("value", value.name).text(value.name)); 
+						}); 
+						
+					}
+				  },
+				});
+				
+		
+				let field1 = this.page.add_field({
+					label: 'Sales Invoice',
+					fieldtype: 'Select',
+					fieldname: 'sales_invoice',
+					options: [],
+					
+				});
+				function get_invoice()
+				{
+					frappe.call({
+						method: 'custom_reports.custom_reports.page.ql_sales_invoice_con.ql_sales_invoice_con.get_invoice_list',
+						args: {'customer':field.get_value()},
+						callback: function (r) {
+						  if (r.message) {
+							  $('[data-fieldname="sales_invoice"][type="text"]').append($("<option></option>").attr("value", "").text("")); 
+							  $.each( r.message.invoices, function( key, value ) {					
+								  
+								  $('[data-fieldname="sales_invoice"][type="text"]').append($("<option></option>").attr("value", value.name).text(value.name)); 
+							  }); 
+							  
+						  }
+						},
+					  });
+				}
+				
+
+				let field4 = this.page.add_field({
+					label: 'View Invoice',
+					fieldtype: 'Button',
+					fieldname: 'create_report',
+					click() {
+						
+						get_report();
+						
+					}
+				});
+				
+				this.page.add_inner_button('Print', () => print_rep());
+				let data='';
+				$(frappe.render_template("laundry_summary",data)).appendTo(this.page.main);
+				
+				function get_report()
+				{
+					//console.log(field.get_value());
+					//console.log(field2.get_value());
+					if(field.get_value() && field1.get_value())
+					{		
+							frappe.call({
+							method: 'custom_reports.custom_reports.page.ql_sales_invoice_con.ql_sales_invoice_con.get_report',
+							freeze: 1,
+							freeze_message: 'Data loading ...please waite',
+							args: {
+							  customer: field.get_value(),
+							  sales_invoice: field1.get_value(),
+							},
+							callback: function (r) {
+							  if (r.message) {
+								  $('#report_egg').html('');
+									
+									let data2=r.message;
+									if(field.get_value()==''){
+										field.set_value(data2.customer);
+									}
+									if(field1.get_value()==''){
+										field1.set_value(data2.sales_invoice);
+									}
+									$(frappe.render_template("laundry_summary_data",data2)).appendTo("#report_egg");
+							  }
+							},
+						  });
+			  
+					}
+				}
+				
+				
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('report_egg');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
---
+++ custom_reports/custom_reports/page/ql_sales_invoice_con/ql_sales_invoice_con.py
@@ -0,0 +1,52 @@
+import frappe
+from frappe.utils import formatdate, getdate, flt
+
+@frappe.whitelist()
+def get_customer_list():
+    data = {}
+    data["customers"] = frappe.get_list("Customer", filters={ 'customer_group': 'Quick Laundry'},fields=['name'],limit_page_length=0, order_by="name",debug=0)
+    return data
+
+@frappe.whitelist()
+def get_invoice_list(customer):
+    data = {}
+    data["invoices"] = frappe.get_list("Sales Invoice", filters={ 'customer': customer,'company':'Quick Laundry – Sole Proprietorship LLC'},fields=['name'],limit_page_length=0, order_by="posting_date desc",debug=0)
+    return data
+    
+@frappe.whitelist()
+def get_report(customer=None,sales_invoice=None):
+    data = {}
+        
+    data['customer']=customer
+    data['sales_invoice']=sales_invoice    
+    data['trn']=frappe.db.get_value("Customer", customer,"tax_id") or ""
+    data['head']=frappe.db.get_value('Letter Head', 'Quick Laundry Letter Head', 'content')
+    inv=frappe.get_doc("Sales Invoice",sales_invoice)
+    inv.posting_date=inv.posting_date.strftime('%d-%m-%Y')
+    inv.in_words=frappe.utils.money_in_words(inv.base_grand_total)
+   
+    data['total']=frappe.utils.fmt_money(inv.total)
+    data['total_taxes_and_charges']=frappe.utils.fmt_money(inv.total_taxes_and_charges)
+    data['grand_total']=frappe.utils.fmt_money(inv.grand_total)
+   
+    data['invoice']=inv
+
+    frappe.db.sql(""" SET @idx=0 """)
+    item=frappe.db.sql(""" select @idx:=@idx+1 AS idx,ad.address_title,sum(si.qty) as qty,sum(si.amount) as amount,sum(si.rate) as rate,si.tax_rate as tax_rate,sum(si.tax_amount) as tax_amount,sum(si.total_amount) as total_amount from `tabSales Invoice Item` si left join `tabDelivery Note` dn on dn.name=si.delivery_note left join `tabAddress` ad on ad.name=dn.customer_address where ad.address_title in ('F & B LINEN','F&B LINEN', 'GUEST LAUNDRY', 'ROOM LINEN', 'RECREATION LINEN', 'STAFF UNIFORM', 'MANAGEMENT LAUNDRY','ADMINISTRATION STAFF') and si.parent='{0}' group by ad.address_title """.format(sales_invoice),as_dict=1,debug=0)   
+    it=[]
+    for itm in item:
+        itm.amount=frappe.utils.fmt_money(itm.amount)
+        itm.rate=frappe.utils.fmt_money(itm.rate)
+        itm.tax_amount=frappe.utils.fmt_money(itm.tax_amount)
+        itm.total_amount=frappe.utils.fmt_money(itm.total_amount)
+        it.append(itm)
+
+    data['items']=it
+
+    return data
+
+       
+    
+
+    
+    
