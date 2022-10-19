e0d1e1132aefbe85cd0490be3b9f6912de76f9f7
jayan13 <jayakumar13s@gmail.com>
2022-10-19 11:51:13 +0530

    clime request

--- custom_reports/api.py
+++ custom_reports/api.py
@@ -11,7 +11,7 @@ from frappe import throw, msgprint, _
 @frappe.whitelist()
 def validate_expense(exp_year,exp_date,employee):
 
-	emprv=frappe.db.sql(""" select * from `tabExpense Request Item` where employee='{0}' and date between DATE_SUB('{2}', INTERVAL {1} YEAR) and '{2}' """.format(employee,exp_year,exp_date),as_dict=1,debug=1)
+	emprv=frappe.db.sql(""" select * from `tabExpense Request Item` where docstatus=1 and employee='{0}' and date between DATE_SUB('{2}', INTERVAL {1} YEAR) and '{2}' """.format(employee,exp_year,exp_date),as_dict=1,debug=1)
 
 	if emprv:
 		return 1
