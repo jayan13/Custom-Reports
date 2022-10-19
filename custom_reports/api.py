1b19d3fdb31400f33c7eeba79b047a29cc1a3134
jayan13 <jayakumar13s@gmail.com>
2022-10-19 20:51:30 +0530

    asset maintanance material request

--- custom_reports/api.py
+++ custom_reports/api.py
@@ -11,10 +11,13 @@ from frappe import throw, msgprint, _
 @frappe.whitelist()
 def validate_expense(exp_year,exp_date,employee):
 
-	emprv=frappe.db.sql(""" select * from `tabExpense Request Item` where docstatus=1 and employee='{0}' and date between DATE_SUB('{2}', INTERVAL {1} YEAR) and '{2}' """.format(employee,exp_year,exp_date),as_dict=1,debug=1)
+	emprv=frappe.db.sql(""" select * from `tabExpense Request Item` where docstatus=1 and employee='{0}' and date between DATE_SUB('{2}', INTERVAL {1} YEAR) and '{2}' """.format(employee,exp_year,exp_date),as_dict=1,debug=0)
 
 	if emprv:
 		return 1
 	else:
 		return 0
 	
+@frappe.whitelist()
+def maintanse_items(sub_gp):
+	return frappe.db.sql(""" select sub_group_items from `tabAsset Sub Group Items` where parent='{0}' """.format(sub_gp),as_dict=1,debug=0)
