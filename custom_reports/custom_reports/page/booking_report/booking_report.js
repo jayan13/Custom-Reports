cbd4c3c1bda5f025a5f68af30394cdde8db64e73
jayan13 <jayakumar13s@gmail.com>
2022-11-01 22:15:02 +0530

    booking report

---
+++ custom_reports/custom_reports/page/booking_report/booking_report.js
@@ -0,0 +1,96 @@
+
+frappe.pages['booking-report'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Booking Report',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				const comarry=[];
+				
+				
+				let field2 = this.page.add_field({
+					label: 'Date From',
+					fieldtype: 'Date',
+					fieldname: 'from_date',
+					
+				});
+		
+				let field3 = this.page.add_field({
+					label: 'Date To',
+					fieldtype: 'Date',
+					fieldname: 'to_date',
+					
+				});
+				let field4 = this.page.add_field({
+					label: 'Create report',
+					fieldtype: 'Button',
+					fieldname: 'create_report',
+					click() {
+						
+						get_report();
+						
+					}
+				});
+				field2.set_value(frappe.datetime.get_today());
+				field3.set_value(frappe.datetime.get_today());		
+				//this.page.add_inner_button('Get  Report', () => get_report());
+				this.page.add_inner_button('Print', () => print_rep());
+				let data='';
+				$(frappe.render_template("booking_report",data)).appendTo(this.page.main);
+				
+				function get_report()
+				{
+					//console.log(field.get_value());
+					//console.log(field2.get_value());
+					if(field2.get_value() && field3.get_value())
+					{		
+							frappe.call({
+							method: 'custom_reports.custom_reports.page.booking_report.booking_report.get_report',
+							freeze: 1,
+							freeze_message: 'Data loading ...please waite',
+							args: {							  
+							  from_date: field2.get_value(),
+							  to_date: field3.get_value(),
+							},
+							callback: function (r) {
+							  if (r.message) {
+								  $('#report_egg').html('');
+									
+									let data2=r.message;									
+									$(frappe.render_template("booking_report_data",data2)).appendTo("#report_egg");
+							  }
+							},
+						  });
+			  
+					}
+				}
+				
+				
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('report_egg');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
---
+++ custom_reports/custom_reports/page/booking_report/booking_report.py
@@ -0,0 +1,57 @@
+import frappe
+from frappe.utils import formatdate, getdate, date_diff
+
+@frappe.whitelist()
+def get_report(from_date=None,to_date=None):
+    data = {}
+    punit=frappe.db.sql(""" select name,contract_start_date,contract_end_date from `tabProperty Unit` where company='AL NOKHBA BUILDING' 
+        and property_name='AL NOKHBA BUILDING' order by cast(unit_no as unsigned)""",as_dict=1,debug=0)
+    data['from_date']=getdate(from_date).strftime('%d/%m/%Y')
+    data['to_date']=getdate(to_date).strftime('%d/%m/%Y')
+    endday=frappe.utils.getdate(to_date)
+    dfrm=frappe.utils.getdate(from_date)
+    data['cnt']=date_diff(endday,dfrm)+1
+    datelist=frappe.utils.add_days(dfrm, -1)
+    html='<tr><td style="width:190px;"></td>'
+    while datelist < endday:
+        datelist=frappe.utils.add_days(datelist,1)
+        html+='<td >'+datelist.strftime('%d/%m')+'</td>'
+    html+='</tr>'
+    for unit in punit:
+        html+='<tr><td style="width:190px;">'+unit.name+'</td>'
+        booking=[]
+        order=frappe.db.sql(""" select contract_start_date,contract_end_date from `tabSales Order` where company='AL NOKHBA BUILDING' 
+        and property='AL NOKHBA BUILDING' and property_unit='{0}' and docstatus<2 
+        and ((contract_start_date between '{1}' and '{2}') or (contract_end_date between '{1}' and '{2}')) """.format(unit.name,from_date,to_date),as_dict=1,debug=0)
+        for ord in order:
+            cdate=frappe.utils.add_days(getdate(ord.contract_start_date), -1)            
+            while cdate < ord.contract_end_date:
+                cdate=frappe.utils.add_days(cdate,1)
+                booking.append(cdate)
+
+        invoice=frappe.db.sql(""" select contract_start_date,contract_end_date from `tabSales Invoice` where company='AL NOKHBA BUILDING' 
+        and property='AL NOKHBA BUILDING' and property_unit='{0}' and docstatus<2 
+        and ((contract_start_date between '{1}' and '{2}') or (contract_end_date between '{1}' and '{2}')) """.format(unit.name,from_date,to_date),as_dict=1,debug=0)
+        for ord in invoice:
+            cdate=frappe.utils.add_days(getdate(ord.contract_start_date), -1)            
+            while cdate < ord.contract_end_date:
+                cdate=frappe.utils.add_days(getdate(cdate),1)
+                booking.append(cdate)
+
+        datelist=frappe.utils.add_days(dfrm, -1)
+        while datelist < endday:
+            datelist=frappe.utils.add_days(datelist,1)
+            if datelist in booking:
+                html+='<td class="occu">x</td>'
+            else:
+                html+='<td></td>'    
+        html+='</tr>'
+
+    data['items']=html
+    return data
+
+       
+    
+
+    
+    
