0b605d51041ee94be8335b931415c267146fa903
jayan13 <jayakumar13s@gmail.com>
2022-12-15 00:18:07 +0530

    employee clearance sale

---
+++ custom_reports/custom_reports/doctype/clearance_form/clearance_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, alantech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clearance Form', {
+	// refresh: function(frm) {
+
+	// }
+});
