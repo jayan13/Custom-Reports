d56fea857375d6b8962f00cf1d051a330a4dd87f
jayan13 <jayakumar13s@gmail.com>
2022-12-30 15:47:57 +0530

    provision report update

--- custom_reports/custom_reports/report/provision_annual_leave/provision_annual_leave.py
+++ custom_reports/custom_reports/report/provision_annual_leave/provision_annual_leave.py
@@ -55,6 +55,12 @@ def get_columns():
 		"fieldtype": "Data",
 		"label": "Leave Code",	
 		"width": 100
+		},
+		{
+		"fieldname": "basic_salary",
+		"fieldtype": "Data",
+		"label": "Basic Salary ",	
+		"width": 100
 		},		
 		{
 		"fieldname": "gross_salary",
@@ -105,6 +111,18 @@ def get_columns():
 		"width": 80
 		},
 		{
+		"fieldname": "amount_accrued",
+		"fieldtype": "Data",
+		"label": "Amount Accrued",	
+		"width": 100
+		},
+		{
+		"fieldname": "amount_used",
+		"fieldtype": "Data",
+		"label": "Amount Used",	
+		"width": 100
+		},
+		{
 		"fieldname": "amount_balance",
 		"fieldtype": "Data",
 		"label": "Amount Balance",	
@@ -112,6 +130,7 @@ def get_columns():
 		},
  	 ]	
 	    
+		
 	return columns
 
 def get_data(conditions,filters):
@@ -127,6 +146,8 @@ def get_data(conditions,filters):
 	parent_department_emp_tot=0
 	department_name_emp_tot=0
 	#Provision Annual Leave Setting
+	alrules=get_provision_rule(company)
+	
 	for emp in conc:
 		if emp.parent_department != parent_department:
 			parent_department=emp.parent_department
@@ -140,41 +161,130 @@ def get_data(conditions,filters):
 		start_date=emp.date_of_joining
 		openabs=0
 		opnused=0
-		cf_leave=0
-		if emp.openning_entry_date:
+		leave_code='0d'
+		gross_salary=0
+		usedleave=0
+		accrued=0
+		balance=0
+		absents=0
+		basic_salary=0
+		amount_balance=0
+		total_days=0
+		actual_worked=0
+		amount_accrued=0
+		amount_used=0
+		if alrules:
+			for rul in alrules:
+				
+				if emp.openning_entry_date:
+					if (rul.date_from=='None' or (rul.date_from!='None' and getdate(rul.date_from) <= emp.openning_entry_date)) and rul.date_to!='None' and getdate(rul.date_to)>=emp.openning_entry_date:
+						#frappe.msgprint(str(rul.date_from)+' '+str(rul.date_to)+' '+str(emp.openning_entry_date))
+						totaldays=date_diff(emp.openning_entry_date,emp.date_of_joining)+1
+						total_days+=totaldays
+						applicable_earnings_component=get_applicable_components(rul.name)
+						sal=get_total_applicable_component_amount(emp.name, applicable_earnings_component, emp.openning_entry_date)
+						actualworked=totaldays-float(emp.opening_absent)
+						absents+=float(emp.opening_absent)
+						actual_worked+=actualworked
+						accru=round((actualworked/365)*emp.leaves_per_year,4)
+						accrued+=accru
+						leave_code=str(emp.leaves_per_year)+'D'
+						usedleaves=float(emp.opening_used_leaves)
+						usedleave+=usedleaves
+						bala=accru-float(emp.opening_used_leaves)
+						balance+=bala
+						basic_salary=sal
+						#amountbalance=round(((sal*12)/365)*bala,2)
+						#amount_balance+=amountbalance
+						amountaccrued=round(((sal*12)/365)*accru,2)
+						amount_accrued+=amountaccrued
+						amountused=round(((sal*12)/365)*usedleaves,2)
+						amount_used+=amountused
+						amount_balance+=amountaccrued-amountused
+
+					elif getdate(processing_month) > emp.openning_entry_date and rul.date_from <= getdate(processing_month):
+						totleave=emp.leaves_per_year
+						tot_leave=get_leave_no(emp.name,processing_month)
+						if tot_leave:
+							totleave=tot_leave
+						day = getdate(emp.openning_entry_date)
+						start_date = add_days(day, 1)
+						openabs=0
+						opnused=0
+						totaldays=date_diff(processing_month,emp.openning_entry_date)
+						total_days+=totaldays
+						applicable_earnings_component=get_applicable_components(rul.name)
+						gross_salary=get_total_applicable_component_amount(emp.name, applicable_earnings_component, processing_month)
+						absent=getabsents(emp.name,openabs,start_date,processing_month)
+						absents+=absent
+						usedleaves=getused(emp.name,opnused,start_date,processing_month)
+						usedleave+=usedleaves
+						leave_code=str(totleave)+'D'
+						actualworked=totaldays-absent
+						actual_worked+=actualworked
+						accru=round((actualworked/365)*float(totleave),2)						
+						accrued+=accru
+						bala=accru-usedleaves
+						balance+=bala
+						amountaccrued=round(((gross_salary*12)/365)*accru,2)
+						amount_accrued+=amountaccrued
+						amountused=round(((gross_salary*12)/365)*usedleaves,2)
+						amount_used+=amountused
+						amount_balance+=amountaccrued-amountused
+						frappe.msgprint(str(amountaccrued))
+						#amountbalance=round(((gross_salary*12)/365)*balance,2)
+						#amount_balance+=amountbalance
+		else:
+			if emp.openning_entry_date:
+				day = getdate(emp.openning_entry_date)
+				start_date = add_days(day, 1)
+				openabs=float(emp.opening_absent)
+				opnused=float(emp.opening_used_leaves)
+				totleave=get_leave_no(emp.name,processing_month)
+				leaves_per_year=emp.leaves_per_year
+				if totleave:
+					leaves_per_year=totleave
+
 			day = getdate(emp.openning_entry_date)
 			start_date = add_days(day, 1)
 			openabs=emp.opening_absent
 			opnused=emp.opening_used_leaves
-		totleave=get_leave_no(emp.name,processing_month)
-		leaves_per_year=emp.leaves_per_year
-		if totleave:
-			leaves_per_year=totleave
+			total_days=date_diff(processing_month,emp.date_of_joining)+1
+			applicable_earnings_component=[]
+			gross_salary=get_total_applicable_component_amount(emp.name, applicable_earnings_component, processing_month)
+			absents=getabsents(emp.name,openabs,start_date,processing_month)
+			usedleave=getused(emp.name,opnused,start_date,processing_month)
+			leave_code=str(leaves_per_year)+'D'
+			actual_worked=total_days-absents
+			accrued=round((actual_worked/365)*leaves_per_year,2)			
+			balance=accrued-usedleave
+			amount_accrued=round(((gross_salary*12)/365)*accrued,2)
+			amount_used=round(((gross_salary*12)/365)*usedleave,2)
+			amount_balance=amount_accrued-amount_used
+			
+			
+
 		
-		total_days=date_diff(processing_month,emp.date_of_joining)+1
-		gross_salary=get_gross_salary(emp.name,company,processing_month)
-		absents=getabsents(emp.name,openabs,start_date,processing_month)
-		usedleave=getused(emp.name,opnused,start_date,processing_month)
-		leave_code=str(leaves_per_year)+'D'
-		actual_worked=total_days-absents
-		accrued=round((actual_worked/365)*leaves_per_year,4)
-		balance=accrued-usedleave
-		amount_balance=round(((gross_salary*12)/365)*balance,2)
+
 		parent_department_tot+=amount_balance
 		department_name_tot+=amount_balance
 		parent_department_emp_tot+=1
 		department_name_emp_tot+=1
+		
 		emp.update({'parent_department_tot':parent_department_tot})
 		emp.update({'department_name_tot':department_name_tot})
 		emp.update({'parent_department_emp_tot':parent_department_emp_tot})
 		emp.update({'department_name_emp_tot':department_name_emp_tot})
 		emp.update({'total_days':total_days})
+		emp.update({'basic_salary':basic_salary})
 		emp.update({'gross_salary':gross_salary})
 		emp.update({'absent':absents})
 		emp.update({'leave_code':leave_code})
 		emp.update({'actual_worked':actual_worked})
 		emp.update({'accrued':accrued})
 		emp.update({'used':usedleave})
+		emp.update({'amount_accrued':amount_accrued})
+		emp.update({'amount_used':amount_used})
 		emp.update({'balance':balance})
 		emp.update({'amount_balance':amount_balance})
 		data.append(emp)
@@ -337,18 +447,29 @@ def get_applicable_components(rule):
 
 	return applicable_earnings_component
 
-def get_provision_rule(company,processing_month):
-	set = frappe.db.sql(""" select name from `tabProvision Annual Leave Setting` where company='{0}' and 
+def get_provision_rule(company,processing_month='',all=1):
+	if processing_month=='':
+		set = frappe.db.sql(""" select name,date_from,date_to from `tabProvision Annual Leave Setting` where company='{0}'  order by creation """.format(company,processing_month),as_dict=1,debug=0)
+		if not set:
+			set = frappe.db.sql(""" select name,date_from,date_to from `tabProvision Annual Leave Setting` where company=''  order by creation """.format(processing_month),as_dict=1,debug=0)
+	else:
+		set = frappe.db.sql(""" select name,date_from,date_to from `tabProvision Annual Leave Setting` where company='{0}' and 
 	((date_to is null and date_from is not null and date_from <= '{1}') 
 	or (date_from is null and date_to is not null and date_to >= '{1}')
-	or (date_from is not null and date_to is not null and '{1}' between date_from and date_to)) """.format(company,processing_month),as_dict=1,debug=0)
-	if not set:
-		set = frappe.db.sql(""" select name from `tabProvision Annual Leave Setting` where company='' and 
+	or (date_from is not null and date_to is not null and '{1}' between date_from and date_to)) order by creation """.format(company,processing_month),as_dict=1,debug=0)
+		if not set:
+			set = frappe.db.sql(""" select name,date_from,date_to from `tabProvision Annual Leave Setting` where company='' and 
 	((date_to is null and date_from is not null and date_from <= '{0}') 
-	or (date_from is null and date_to is not null and date_to >= '{0}') 	or (date_from is not null and date_to is not null and '{0}' between date_from and date_to) ) """.format(processing_month),as_dict=1,debug=0)
+	or (date_from is null and date_to is not null and date_to >= '{0}') 	or (date_from is not null and date_to is not null and '{0}' between date_from and date_to) ) order by creation """.format(processing_month),as_dict=1,debug=0)
+	
 	if not set:
 		return
-	return set[0].name
+	if all:
+		val=set
+	else:
+		val=set[0]
+
+	return val
 
 def get_leave_no(emp,processing_month):
 	
