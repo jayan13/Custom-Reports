da36761031ddf4f9deadaee1fb9644b5aa4e849e
jayan13 <jayakumar13s@gmail.com>
2023-01-04 13:59:54 +0530

    ticket request

---
+++ custom_reports/custom_reports/doctype/advance_air_ticket_request/advance_air_ticket_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, alantech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Advance Air Ticket Request', {
+	// refresh: function(frm) {
+
+	// }
+});
