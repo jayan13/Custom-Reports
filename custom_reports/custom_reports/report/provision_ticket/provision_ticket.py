dbd1108076df639f3ce0a5565024323bebcdbbfb
jayan13 <jayakumar13s@gmail.com>
2023-01-30 23:30:04 +0530

    provision ticket

--- custom_reports/custom_reports/report/provision_ticket/provision_ticket.py
+++ custom_reports/custom_reports/report/provision_ticket/provision_ticket.py
@@ -371,7 +371,7 @@ def get_data(conditions,filters):
 		amount_used=round(amount_used,2)
 		#amount_balance=round(amount_balance,2)
 		amount_balance=round(amount_accrued-amount_used,2)
-		years+=round((actual_worked/365),3)
+		#years+=round((actual_worked/365),3)
 		years=round(years,3)
 
 		parent_department_tot+=amount_balance
