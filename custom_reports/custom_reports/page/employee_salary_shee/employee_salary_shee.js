e2c211d9a31f1b3bb29a91c0a7410ac803227869
jayan13 <jayakumar13s@gmail.com>
2023-02-20 01:47:53 +0530

    salary sheet page

---
+++ custom_reports/custom_reports/page/employee_salary_shee/employee_salary_shee.js
@@ -0,0 +1,52 @@
+
+frappe.pages['employee-salary-shee'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+	
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Employee Salary Sheet Report',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				
+				this.page.add_inner_button('Print', () => print_rep());
+				var wrp=this.page.main
+				frappe.call({
+					method: 'custom_reports.custom_reports.page.employee_salary_shee.employee_salary_shee.get_report',
+					freeze: 1,
+					freeze_message: 'Data loading ...please waite',
+					args: {							  
+						payroll_entry: frappe.get_route()[1],
+					  },					
+					callback: function (r) {
+					  if (r.message) {							
+							$(frappe.render_template("employee_salary_shee",r.message)).appendTo(wrp);	
+					  }
+					},
+				  });
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('report_eggs');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
---
+++ custom_reports/custom_reports/page/employee_salary_shee/employee_salary_shee.py
@@ -0,0 +1,147 @@
+import frappe
+from frappe.utils import formatdate, getdate, date_diff
+
+@frappe.whitelist()
+def get_report(payroll_entry=None):
+    data = {}
+    payro=frappe.db.get_value("Payroll Entry",payroll_entry,['start_date','end_date','company'], as_dict=1)
+    data['company']=payro.company
+    data['start_date']=frappe.utils.formatdate(payro.start_date, "MMMM yyyy")
+    data['end_date']=frappe.utils.formatdate(payro.end_date, "MMMM yyyy")    
+    
+    slip=frappe.db.sql(""" select s.name,s.employee,s.gross_pay,s.net_pay,s.total_deduction,s.leave_without_pay,s.payment_days,
+    d.name as department,IF(d.parent_department='All Departments',d.name,d.parent_department) as parent_department 
+    from `tabSalary Slip` s left join `tabEmployee` e on e.name=s.employee 
+    left join `tabDepartment` d on d.name=e.department where s.payroll_entry='{0}'  order by d.parent_department,d.name """.format(payroll_entry),as_dict=1,debug=0)
+    slips=[]
+    emp_count=0
+    emp_count_dept=0
+    emp_count_pare_dept=0
+    parent_department_name=''
+    department_name=''
+    parent_department_tot=0
+    parent_department_ern_tot=0
+    parent_department_ded_tot=0
+    department_tot=0
+    department_ern_tot=0
+    department_ded_tot=0
+    if slip:
+        for slp in slip:
+            department=slp.department.split('-')[0]
+            parent_department=slp.parent_department.split('-')[0]
+            if parent_department != parent_department_name:
+                parent_department_name=parent_department
+                parent_department_tot=0
+                parent_department_ern_tot=0
+                parent_department_ded_tot=0			
+                emp_count_pare_dept=0
+                parent_department_basic_tot=0
+                parent_department_basic_pay_tot=0
+                parent_department_allowance_tot=0
+                
+
+            if department != department_name:
+                department_name=department
+                department_tot=0
+                department_ern_tot=0
+                department_ded_tot=0			
+                emp_count_dept=0
+                department_basic_tot=0
+                department_basic_pay_tot=0
+                department_allowance_tot=0
+
+            emp_count+=1
+            emp_count_pare_dept+=1
+            emp_count_dept+=1
+            dt={}
+            dt.update({'slip':slp.name})
+            dt.update({'employee':slp.employee})
+            dt.update({'employee_name':slp.employee_name})
+            dt.update({'department':department_name})
+            dt.update({'parent_department':parent_department_name})
+            dt.update({'gross_pay':slp.gross_pay})
+            dt.update({'net_pay':slp.net_pay})
+            dt.update({'total_deduction':slp.total_deduction})
+            dt.update({'leave_without_pay':slp.leave_without_pay})				
+            dt.update({'payment_days':slp.payment_days})
+				
+            paid_leaves=get_paid_leave(slp.employee,payro.end_date)
+            dt.update({'paid_leaves':paid_leaves})
+            dt.update({'over_time':slp.over_time})
+            dt.update({'holiday_over_time':slp.holiday_over_time})
+
+            parent_department_tot+=float(slp.net_pay or 0)
+            parent_department_ern_tot+=float(slp.gross_pay or 0)
+            parent_department_ded_tot+=float(slp.total_deduction or 0)
+            department_tot+=float(slp.net_pay or 0)
+            department_ern_tot+=float(slp.gross_pay or 0)
+            department_ded_tot+=float(slp.total_deduction or 0)
+            dt.update({'emp_count':emp_count})
+            dt.update({'emp_count_pare_dept':emp_count_pare_dept})
+            dt.update({'emp_count_dept':emp_count_dept})
+            dt.update({'parent_department_tot':parent_department_tot})
+            dt.update({'parent_department_ern_tot':parent_department_ern_tot})
+            dt.update({'parent_department_ded_tot':parent_department_ded_tot})
+            dt.update({'department_tot':department_tot})
+            dt.update({'department_ern_tot':department_ern_tot})
+            dt.update({'department_ded_tot':department_ded_tot})
+
+            base=frappe.db.get_value('Salary Structure Assignment',{'salary_structure':slp.salary_structure,'employee':slp.employee},'base') or 0
+            dt.update({'basic':base})
+            earnings=[]
+            deductions=[]
+            earnin=frappe.db.sql(""" select salary_component,amount from `tabSalary Detail` where parentfield='earnings' and parent ='{0}' """.format(slp.name),as_dict=1,debug=0)
+            basic_paid=0
+            if earnin:
+                for ern in earnin:
+                    if 'Basic' in str(ern.salary_component):
+                        basic_paid=ern.amount 
+                    else:
+                        earnings.append(ern)
+
+            dt.update({'earnings':earnings})
+            deduct=frappe.db.sql(""" select salary_component,amount from `tabSalary Detail` where parentfield='deductions' and parent ='{0}' """.format(slp.name),as_dict=1,debug=0)
+            if deduct:
+                deductions=deduct
+            dt.update({'deductions':deductions})
+            dt.update({'basic_paid':basic_paid})
+            allowance=float(slp.gross_pay)-float(basic_paid)
+            dt.update({'allowance':allowance})
+            department_basic_tot+=base
+            department_basic_pay_tot+=basic_paid
+            department_allowance_tot+=allowance
+            parent_department_basic_tot+=base
+            parent_department_basic_pay_tot+=basic_paid
+            parent_department_allowance_tot+=allowance
+            dt.update({'department_basic_tot':department_basic_tot})
+            dt.update({'department_basic_pay_tot':department_basic_pay_tot})
+            dt.update({'department_allowance_tot':department_allowance_tot})
+            dt.update({'parent_department_basic_tot':parent_department_basic_tot})
+            dt.update({'parent_department_basic_pay_tot':parent_department_basic_pay_tot})
+            dt.update({'parent_department_allowance_tot':parent_department_allowance_tot})
+            slips.append(dt)
+    data['data']=slips
+    return data
+
+def get_paid_leave(emp,sal_date):
+	absent=0
+	sal3=frappe.db.sql(""" select count(*) as absent FROM `tabAttendance` a left join `tabLeave Type` l on l.name=a.leave_type 
+	where a.docstatus=1 and a.status='On Leave' and a.employee='{0}' and MONTH(a.attendance_date)=MONTH('{1}') and YEAR(a.attendance_date)=YEAR('{1}') and l.is_ppl='1' """.format(emp,sal_date),as_dict=1,debug=0)
+	if sal3:
+		absent+=float(sal3[0].absent)/2
+	
+	sal4=frappe.db.sql(""" select count(*) as absent FROM `tabAttendance` a left join `tabLeave Type` l on l.name=a.leave_type 
+	where a.docstatus=1 and a.status='On Leave' and a.employee='{0}' and MONTH(a.attendance_date)=MONTH('{1}') and YEAR(a.attendance_date)=YEAR('{1}') and l.is_lwp='0' and l.is_ppl='0' """.format(emp,sal_date),as_dict=1,debug=0)
+	if sal4:
+		absent+=sal4[0].absent
+	
+	sal6=frappe.db.sql(""" select count(*) as absent FROM `tabAttendance` a left join `tabLeave Type` l on l.name=a.leave_type 
+	where a.docstatus=1 and a.status='Half Day' and a.employee='{0}' and MONTH(a.attendance_date)=MONTH('{1}') and YEAR(a.attendance_date)=YEAR('{1}') and l.is_lwp='0' and l.is_ppl='0'  """.format(emp,sal_date),as_dict=1,debug=0)
+	if sal6:
+		absent+=float(sal6[0].absent)/2
+	
+	return absent       
+    
+
+    
+    
