1bdd04fd934daee46e60b931be5e26feaca9c678
jayan13 <jayakumar13s@gmail.com>
2023-02-20 22:10:59 +0530

    annnual leave slip

--- custom_reports/custom_reports/doctype/annual_leave_payslip/annual_leave_payslip.py
+++ custom_reports/custom_reports/doctype/annual_leave_payslip/annual_leave_payslip.py
@@ -6,14 +6,17 @@ from frappe.model.document import Document
 
 class AnnualLeavePayslip(Document):
 	def on_submit(self):
+		frappe.msgprint('hai')
 		#doc = frappe.get_doc('Leave Application', self.leave_application)
 		#doc.salary_paid_in_advance = '1'
 		#doc.save()
 		frappe.db.set_value('Leave Application', self.leave_application, 'salary_paid_in_advance',1)
 		#frappe.msgprint('hai')
+
 	def on_cancel(self):
 		#doc = frappe.get_doc('Leave Application', self.leave_application)
 		#doc.salary_paid_in_advance = '0'
 		#doc.save()	
 		frappe.db.set_value('Leave Application', self.leave_application, 'salary_paid_in_advance',0)
 		#frappe.msgprint('hai')
+
