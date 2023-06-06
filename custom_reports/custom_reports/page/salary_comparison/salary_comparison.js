0c51caa837a1e6bd7c69057c96e71ef77a3a399a
jayan13 <jayakumar13s@gmail.com>
2023-06-06 14:28:37 +0530

    salary comparison

---
+++ custom_reports/custom_reports/page/salary_comparison/salary_comparison.js
@@ -0,0 +1,77 @@
+
+frappe.pages['salary-comparison'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+	
+}
+
+		MyPage =Class.extend({
+	
+			init: function(wrapper){
+					this.page = frappe.ui.make_app_page({
+					parent: wrapper,
+					title: 'Salary Comparison',
+					single_column: true
+				});
+					this.make();
+			},
+			make: function()
+			{
+				frappe.get_route()[1]
+				let field = this.page.add_field({
+					label: 'Company',
+					fieldtype: 'Link',
+					fieldname: 'company',
+					options: 'Company',
+					change() {
+						get_report();
+					}
+				});
+			this.page.add_inner_button('Print', () => print_rep());
+			var wrp=this.page.main
+
+			if(frappe.get_route()[1]){
+				field.set_value(frappe.get_route()[1]);
+				get_report();
+			}
+			
+			function get_report()
+			{
+				$('#report_eggs').remove();
+				if(field.get_value())
+				{
+					
+					frappe.call({
+						method: 'custom_reports.custom_reports.page.salary_comparison.salary_comparison.get_report',
+						freeze: 1,
+						freeze_message: 'Data loading ...please waite',
+						args: {							  
+							company: field.get_value(),
+						},					
+						callback: function (r) {
+						if (r.message) {							
+								$(frappe.render_template("salary_comparison",r.message)).appendTo(wrp);	
+						}
+						},
+					}); 
+				}
+			}
+				
+				
+				
+			}
+		
+		
+		})
+		
+		function print_rep()
+				{
+					
+					var divToPrint=document.getElementById('report_eggs');
+		
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><style>	.table-bordered{ display: table; text-indent: initial; border-spacing: 0; border-collapse: collapse; width: 100%; max-width: 100%; font-size: inherit; } .table-bordered tr td{ border-color: black !important; border-left:none !important; border-right:none !important; } .table-bordered{ border:none !important; } </style><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+		  
+				}
---
+++ custom_reports/custom_reports/page/salary_comparison/salary_comparison.py
@@ -0,0 +1,146 @@
+import frappe
+from frappe.utils import formatdate, getdate, date_diff, flt,get_first_day,get_last_day,add_days
+
+@frappe.whitelist()
+def get_report(company=None):
+    data = {}
+    payro={}
+    payrosql=frappe.db.get_all("Payroll Entry", filters={'company':company}, fields=['start_date','end_date','name','company'], order_by='start_date desc', start=0, page_length=1,debug=0)
+    
+    if payrosql:
+        payro=payrosql[0]
+    else:
+        return data
+
+    months=[]
+    payroll_entry=payro.name
+    data['company']=payro.company
+    data['start_date']=frappe.utils.formatdate(payro.start_date, "MMMM yyyy")
+    data['end_date']=frappe.utils.formatdate(payro.end_date, "MMMM yyyy")    
+    months.append(frappe.utils.formatdate(payro.start_date, "MMMM yyyy"))
+    prvmth=add_days(get_first_day(getdate(payro.start_date)),-1)
+    pprvmth=add_days(get_first_day(getdate(prvmth)),-1)
+
+    months.append(frappe.utils.formatdate(prvmth, "MMMM yyyy"))
+    months.append(frappe.utils.formatdate(pprvmth, "MMMM yyyy"))
+    data['months']=months
+    
+    slip=frappe.db.sql(""" select s.name,s.employee,s.salary_structure,s.employee_name,s.gross_pay,s.net_pay,s.total_deduction,s.leave_without_pay,s.payment_days,s.over_time,s.holiday_over_time,
+    d.name as department,IF(d.parent_department='All Departments',d.name,d.parent_department) as parent_department 
+    from `tabSalary Slip` s 
+    left join `tabDepartment` d on d.name=s.department where  s.docstatus in (0,1) and s.payroll_entry='{0}'  order by d.parent_department,d.name """.format(payroll_entry),as_dict=1,debug=0)
+    
+    compres=frappe.db.sql(""" select rmc.label,GROUP_CONCAT(rmc.salary_component) as salary_component from `tabSalary Sheet Report Settings` re left join `tabSalary Report Removed Components` rmc on re.name=rmc.parent where re.company='{0}' group by rmc.label order by rmc.display_order """.format(payro.company),as_dict=1,debug=0)	
+    
+    slips=[]
+
+    emp_count=0
+    emp_count_dept=0
+    emp_count_pare_dept=0
+    parent_department_name=''
+    department_name=''
+    
+    if slip:
+        for slp in slip:
+            department=slp.department.split('-')[0] if slp.department else ''
+            parent_department=slp.parent_department.split('-')[0] if slp.parent_department else ''
+            if parent_department != parent_department_name:
+                parent_department_name=parent_department               	
+                emp_count_pare_dept=0
+
+            if department != department_name:
+                department_name=department                              			
+                emp_count_dept=0
+                
+            dt={}
+            dt.update({'slip':slp.name})
+            dt.update({'employee':slp.employee})
+            dt.update({'employee_name':slp.employee_name})
+            dt.update({'department':department_name})
+            dt.update({'parent_department':parent_department_name})
+							
+            earnin=frappe.db.sql(""" select salary_component,amount from `tabSalary Detail` where parentfield='earnings' and parent ='{0}' """.format(slp.name),as_dict=1,debug=0)
+            overtime=0
+            sal=0
+            if compres:
+                for comp in compres:
+                    cmplist=comp.get("salary_component")
+                    cmplistar=cmplist.split(",")
+                    if earnin:
+                        for ern in earnin:
+                            if ern.salary_component in cmplistar:
+                                overtime+=ern.amount
+                            
+            sal=slp.net_pay-overtime                    
+            sal=flt(sal,2)
+            overtime=flt(overtime,2)
+            
+            dt.update({'overtime':overtime})
+            dt.update({'sal':sal})
+            p1=frappe.db.sql(""" select name,net_pay from `tabSalary Slip` where  docstatus in (0,1) and employee='{0}' and MONTH(start_date)=MONTH('{1}') and YEAR(start_date)=YEAR('{1}') """.format(slp.employee,prvmth),as_dict=1,debug=0)
+            if p1:
+                earnin=frappe.db.sql(""" select salary_component,amount from `tabSalary Detail` where parentfield='earnings' and parent ='{0}' """.format(p1[0].name),as_dict=1,debug=0)
+                overtime=0
+                sal=0
+                if compres:
+                    for comp in compres:
+                        cmplist=comp.get("salary_component")
+                        cmplistar=cmplist.split(",")
+                        if earnin:
+                            for ern in earnin:
+                                if ern.salary_component in cmplistar:
+                                    overtime+=ern.amount
+                                
+                sal=p1[0].net_pay-overtime                    
+                sal=flt(sal,2)
+                overtime=flt(overtime,2)
+                dt.update({'overtimep':overtime})
+                dt.update({'salp':sal})
+
+            else:
+                dt.update({'overtimep':0})
+                dt.update({'salp':0})
+
+            p2=frappe.db.sql(""" select name,net_pay from `tabSalary Slip` where  docstatus in (0,1) and employee='{0}' and MONTH(start_date)=MONTH('{1}') and YEAR(start_date)=YEAR('{1}')  """.format(slp.employee,pprvmth),as_dict=1,debug=0)
+            if p2:
+                earnin=frappe.db.sql(""" select salary_component,amount from `tabSalary Detail` where parentfield='earnings' and parent ='{0}' """.format(p2[0].name),as_dict=1,debug=0)
+                overtime=0
+                sal=0
+                if compres:
+                    for comp in compres:
+                        cmplist=comp.get("salary_component")
+                        cmplistar=cmplist.split(",")
+                        if earnin:
+                            for ern in earnin:
+                                if ern.salary_component in cmplistar:
+                                    overtime+=ern.amount
+                                
+                sal=p2[0].net_pay-overtime                    
+                sal=flt(sal,2)
+                overtime=flt(overtime,2)
+                dt.update({'overtimepp':overtime})
+                dt.update({'salpp':sal})
+
+            else:
+                dt.update({'overtimepp':0})
+                dt.update({'salpp':0})
+            
+            emp_count+=1
+            emp_count_pare_dept+=1
+            emp_count_dept+=1
+            dt.update({'emp_count':emp_count})
+            dt.update({'emp_count_pare_dept':emp_count_pare_dept})
+            dt.update({'emp_count_dept':emp_count_dept})
+            slips.append(dt)
+        
+        
+
+    data['data']=slips
+    #frappe.throw(str(data))
+    return data
+
+      
+    
+
+    
+    
